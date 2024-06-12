64b5d39ca72d1be84a5d62b40c434e634e2ed9e3
orinamae <spore2078@gmail.com>
2024-06-13 01:16:19 +0700

    Update PDFViewer.js

--- src/components/PDFViewer.js
+++ src/components/PDFViewer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
-import pdfFile from '../pdf/english.pdf'; // импорт pdf
+import pdfFile from '../pdf/textbook_1.pdf'; // импорт pdf
 pdfjs.GlobalWorkerOptions.workerSrc = `file://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const PDFViewer = () => {
